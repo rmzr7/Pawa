3afe6fe7218e9ae4391ad2a7e5aa1ff0c4bc619a
harveybia <harvey.shi@yahoo.com>
2015-10-17 17:00:32 -0400

    Fixed some part, uses most paths.

--- src/game/player.py
+++ src/game/player.py
@@ -15,8 +15,6 @@ class Player(BasePlayer):
     has_built_station = False
 
     def __init__(self, state):
-        self.virtgraph = state.get_graph().copy()
-        self.pastActiveOrders = state.get_active_orders()
         """
         Initializes your Player. You can set up persistent state, do analysis
         on the input graph, engage in whatever pre-computation you need. This
@@ -50,20 +48,21 @@ class Player(BasePlayer):
     def determine_fufill_order(self, path):
         pass
 
-    def removePath(self, path):
-        for i in range(len(path)):
-            if i + 1 < len(path):
-                self.virtgraph.remove_edge(path[i], path[i + 1])
-
-
-
-
     def key_with_max_val(self, d):
         """ a) create a list of the dict's keys and values;
             b) return the key with the max value"""
         v=list(d.values())
         k=list(d.keys())
         return k[v.index(max(v))]
+
+    def get_graph_without_orders(self, state, graph):
+        new_graph = graph.copy()
+        for order_with_path in state.get_active_orders():
+            path = order_with_path[1]
+            for index in range(len(path) - 1):
+                new_graph.remove_edge(path[index], path[index + 1])
+
+        return new_graph
 
     def step(self, state):
         """
@@ -91,32 +90,17 @@ class Player(BasePlayer):
             commands.append(self.build_command(station))
             self.has_built_station = True
 
-
-        for pastActiveOrder in self.pastActiveOrders:
-            if pastActiveOrder not in state.get_active_orders():
-                self.addPath(pastActiveOrder[1])
-
-
         pending_orders = state.get_pending_orders()
         selections = {} # {int:order}
         if len(pending_orders) != 0:
             for order in pending_orders:
-                path = nx.shortest_path(self.virtgraph, station, order.get_node())
+                path = nx.shortest_path(self.get_graph_without_orders(state, graph), station, order.get_node())
                 selections[self.get_actual_gain(state, order, path)] = order
             opt_order = selections[self.key_with_max_val(selections)]
 
             #order = random.choice(pending_orders)
-            path = nx.shortest_path(self.virtgraph, station, opt_order.get_node())
+            path = nx.shortest_path(self.get_graph_without_orders(state, graph), station, opt_order.get_node())
             if self.path_is_valid(state, path) and self.get_actual_gain(state, order, path) <= 90:
                 commands.append(self.send_command(opt_order, path))
-                self.removePath(path)
-        self.pastActiveOrders = state.get_active_orders()
+
         return commands
-
-    def addPath(self, path):
-        for i in range(len(path)):
-            if i + 1 < len(path):
-                self.virtgraph.add_edge(path[i], path[i + 1])
-
-
-
